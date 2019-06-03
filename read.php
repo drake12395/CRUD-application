d2a586cc42cdaf3e751c193d8d9f49e1bd93ab6a
Drake Schmidt <40924549+drake12395@users.noreply.github.com>
2019-06-02 20:16:15 -0400

    Update read.php

--- read.php
+++ read.php
@@ -29,42 +29,40 @@
 <body>
     <div class="container">
      
-                <div class="span10 offset1">
-                    <div class="row">
-                        <h3>Read a Customer</h3>
-                    </div>
+         <div class="span10 offset1">
+             <div class="row">
+                 <h3>Read a Customer</h3>
+              </div>
                      
-                    <div class="form-horizontal" >
-                      <div class="control-group">
-                        <label class="control-label">Name</label>
-                        <div class="controls">
-                            <label class="checkbox">
-                                <?php echo $data['name'];?>
-                            </label>
+              <div class="form-horizontal" >
+                  <div class="control-group">
+                      <label class="control-label">Name</label>
+                       <div class="controls">
+                           <label class="checkbox">
+                           <?php echo $data['name'];?>
+                        </label>
                         </div>
-                      </div>
+                   </div>
                       <div class="control-group">
-                        <label class="control-label">Email Address</label>
-                        <div class="controls">
-                            <label class="checkbox">
-                                <?php echo $data['email'];?>
-                            </label>
-                        </div>
+                          <label class="control-label">Email Address</label>
+                          <div class="controls">
+                              <label class="checkbox">
+                              <?php echo $data['email'];?>
+                              </label>
+                           </div>
                       </div>
-                      <div class="control-group">
+                    <div class="control-group">
                         <label class="control-label">Mobile Number</label>
                         <div class="controls">
                             <label class="checkbox">
                                 <?php echo $data['mobile'];?>
-                            </label>
+                         </label>
                         </div>
-                      </div>
+                     </div>
                         <div class="form-actions">
                           <a class="btn" href="index.php">Back</a>
-                       </div>
-                     
-                      
-                    </div>
+                        </div>                          
+                     </div>
                 </div>
                  
     </div> <!-- /container -->
